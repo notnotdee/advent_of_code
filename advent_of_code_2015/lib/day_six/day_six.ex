5c3f1afe686e5c64a8e73936f11351780d9b0746
Dee Watson <deewatson@newrelic.com>
2022-12-14 19:46:13 -0500

    Day six in progress

--- advent_of_code_2015/lib/day_six/day_six.ex
+++ advent_of_code_2015/lib/day_six/day_six.ex
@@ -1,7 +1,6 @@
 defmodule DaySix do
   @puzzle_input File.read!("lib/day_six/day_six_input.txt")
                 |> String.split("\n", trim: true)
-                |> Enum.take(1)
 
   def parse_input do
     @puzzle_input
@@ -9,14 +8,31 @@ defmodule DaySix do
     |> Enum.map(&format_input/1)
   end
 
+  def build_map do
+    cartesian_product(0..4, 0..4)
+    |> Map.new(&{&1, false})
+  end
+
+  def part_one do
+  end
+
+  #
+
   def format_input([_, "on", lo, _, hi]), do: to_format(:turn_on, lo, hi)
   def format_input([_, "off", lo, _, hi]), do: to_format(:turn_off, lo, hi)
   def format_input(["toggle", lo, _, hi]), do: to_format(:toggle, lo, hi)
 
+  @spec to_format(any, binary, binary) :: {any, {any, any}, {any, any}}
   def to_format(instruction, lo, hi) do
     [x1, y1] = lo |> String.split(",") |> Enum.map(&String.to_integer/1)
     [x2, y2] = hi |> String.split(",") |> Enum.map(&String.to_integer/1)
 
     {instruction, {x1, y1}, {x2, y2}}
+  end
+
+  def cartesian_product(set1, set2) do
+    for elem1 <- set1, elem2 <- set2 do
+      {elem1, elem2}
+    end
   end
 end
